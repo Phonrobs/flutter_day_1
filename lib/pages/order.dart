27caab96cec7d23bbba8da80dc35578edcb71756
Phonrob Sawasdee <phonrob.s@up.ac.th>
2019-02-27 09:48:59 +0700

    v27-1

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -12,19 +12,40 @@ class OrderPage extends StatefulWidget {
 }
 
 class OrderPageState extends State<OrderPage> {
-  var foodNameController =TextEditingController();
+  var foodNameController = TextEditingController();
   var formKey = GlobalKey<FormState>();
+  var menuIndex = -1;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('New Order'),
-      ),
-      body: Form(
+  void showMenu(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return GridView.count(
+            crossAxisCount: 2,
+          );
+        });
+  }
+
+  Widget buildMenuButton(BuildContext context) {
+    if (menuIndex == -1) {
+      return Padding(
+          padding: EdgeInsets.all(20.0),
+          child: OutlineButton(
+            child: Text('Select a menu'),
+            onPressed: () => showMenu(context),
+          ));
+    } else {
+      return Container();
+    }
+  }
+
+  Widget buildForm(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
         key: formKey,
         child: Column(
           children: [
+            buildMenuButton(context),
             Padding(
               padding: EdgeInsets.all(20.0),
               child: TextFormField(
@@ -32,7 +53,7 @@ class OrderPageState extends State<OrderPage> {
                 decoration: InputDecoration(labelText: 'ชื่ออาหาร'),
                 autofocus: true,
                 validator: (value) {
-                  if(value == null || value.isEmpty) {
+                  if (value == null || value.isEmpty) {
                     return 'โปรดกรอกชื่ออาหารให้ถูกต้อง';
                   }
                 },
@@ -45,9 +66,9 @@ class OrderPageState extends State<OrderPage> {
                   child: OutlineButton(
                     child: Text('Save'),
                     onPressed: () {
-                      if(formKey.currentState.validate()) {
-                         widget.addOrder(foodNameController.text);
-                         Navigator.pop(context);
+                      if (formKey.currentState.validate()) {
+                        widget.addOrder(foodNameController.text);
+                        Navigator.pop(context);
                       }
                     },
                   ),
@@ -64,5 +85,14 @@ class OrderPageState extends State<OrderPage> {
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('New Order'),
+        ),
+        body: buildForm(context));
   }
 }
